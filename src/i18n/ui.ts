a4e53cd34c760e6cba8b24a9044b65eadde9633b
salvatorelaspata <salvatore.la.spata@gmail.com>
2024-01-20 21:53:02 +0100

    refactoring and content improvment

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: 'English',
+  it: 'Italiano',
+};
+
+export const defaultLang = 'it';
+
+export const ui = {
+  en: {
+    'landing.main.hi': 'hi',
+    'landing.about': 'About',
+    'landing.twitter': 'Twitter',
+  },
+  it: {
+    'landing.main.hi': 'hi',
+    'landing.about': 'About',
+    'landing.twitter': 'Twitter',
+  },
+} as const;
