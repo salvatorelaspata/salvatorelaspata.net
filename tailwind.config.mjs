d33960530e3097474d986b33e4f1bb3aed24cd80
SalvatoreLa <salvatore.la.spata@gmail.com>
2024-08-02 22:14:27 +0200

    a little colors and a little animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,15 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+        palette1: "#002B39",
+        palette2: "#004A58",
+        palette3: "#006B7A",
+        palette4: "#358E9D",
+        palette5: "#5EB2C2",
+			}
+		},
 	},
 	plugins: [],
 }
